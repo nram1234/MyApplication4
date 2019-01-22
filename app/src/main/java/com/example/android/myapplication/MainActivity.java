3e530f3b3dc32425c87dafce3479d159b9bb086b
nram1234 <redanaser0@gmail.com>
2019-01-22 13:27:00 +0200

    try

---
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void test(){
+        int i =1*3;
+    }
+}
